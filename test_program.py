92327596ac7f12d65666e851c93a76514f328274
Олег Малыгин <malyginoleg1979@gmail.com>
2026-09-24 00:06:01 +0300

    Add first commit

--- test_program.py
+++ test_program.py
@@ -5,7 +5,7 @@ dir_files = [filename.lower() for filename in os.listdir(BASE_DIR)]
 
 files_list = ['program.py', 'readme.md']
 
-
+# Добавил коммент
 def test_program():
     for filename in files_list:
         assert filename in dir_files, f'Файл `{filename}` не найден.'
